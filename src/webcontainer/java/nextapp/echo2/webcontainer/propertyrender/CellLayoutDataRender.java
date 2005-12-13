62b149fd46b46bac82380e0362c4d5e6954c5bf3
tliebeck <tliebeck@7cdfe670-c0f4-0310-b952-83ccaed83b6f>
2005-12-13 10:34:19 +0000

    Fixed cell layout data rendering bug where stylesheets were being ignored.

--- src/webcontainer/java/nextapp/echo2/webcontainer/propertyrender/CellLayoutDataRender.java
+++ src/webcontainer/java/nextapp/echo2/webcontainer/propertyrender/CellLayoutDataRender.java
@@ -71,7 +71,7 @@ public class CellLayoutDataRender {
             for (int i = 0; i < childCount; ++i) {
                 Component child = component.getComponent(i);
                 if (child.getRenderId().equals(childRenderId)) {
-                    return ((CellLayoutData) child.getLayoutData()).getBackgroundImage().getImage();
+                    return ((CellLayoutData) child.getProperty(Component.PROPERTY_LAYOUT_DATA)).getBackgroundImage().getImage();
                 }
             }
         }
@@ -100,7 +100,7 @@ public class CellLayoutDataRender {
      */
     public static void renderBackgroundImageToStyle(CssStyle cssStyle, RenderContext rc, ImageRenderSupport irs,
             Component parent, Component child) {
-        CellLayoutData layoutData = (CellLayoutData) child.getLayoutData();
+        CellLayoutData layoutData = (CellLayoutData) child.getProperty(Component.PROPERTY_LAYOUT_DATA);
         if (layoutData == null || layoutData.getBackgroundImage() == null) {
             return;
         }
